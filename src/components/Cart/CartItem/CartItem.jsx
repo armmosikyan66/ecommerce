f152442b39716f57310c1fd82549196e37d97c3d
Arman <87537406+armmosikyan66@users.noreply.github.com>
2022-05-23 17:48:10 +0400

    Update CartItem.jsx

---
+++ src/components/Cart/CartItem/CartItem.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Typography, Button, Card, CardActions, CardContent, CardMedia} from "@material-ui/core";
+import useStyles from './styles'
+
+const CartItem = () => {
+
+    const classes = useStyles();
+
+    return (
+        <Card>
+            <CardMedia image={item.media.source} />
+        </Card>
+    );
+};
+
+export default CartItem;
